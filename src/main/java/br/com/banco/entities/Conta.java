c3875d0b691432ba866868f691a558dccd7191ff
maurilosantos <maurilosantoss1@gmail.com>
2023-07-16 13:15:02 -0300

    build: tabelas Conta, Transferencia

---
+++ src/main/java/br/com/banco/entities/Conta.java
@@ -0,0 +1,18 @@
+package br.com.banco.entities;
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "conta")
+@Data
+public class Conta {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_conta")
+    private Long idConta;
+
+    @Column(name = "nome_responsavel", nullable = false)
+    private String nomeResponsavel;
+}
+
---
+++ src/main/java/br/com/banco/entities/Transferencia.java
@@ -0,0 +1,32 @@
+package br.com.banco.entities;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "transferencia")
+@Data
+public class Transferencia {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "data_transferencia", nullable = false)
+    private LocalDateTime dataTransferencia;
+
+    @Column(name = "valor", nullable = false, precision = 20, scale = 2)
+    private BigDecimal valor;
+
+    @Column(name = "tipo", nullable = false)
+    private String tipo;
+
+    @Column(name = "nome_operador_transacao")
+    private String nomeOperadorTransacao;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "conta_id", nullable = false)
+    private Conta conta;
+}
